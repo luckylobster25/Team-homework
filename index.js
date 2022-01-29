c0022b677e9f81943bca42f4746d763e48b47520
Chue Chang <chuechang1996@outlook.com>
2022-01-29 14:50:38 -0600

    add main prompt and finsh up other prompt

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const team = require('./src/page-template');
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 let teamMember = [];
 
-let managerPrompt =
+function mainPrompt() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Choose to add employee",
+            name: "addEmployee",
+            choices: ["Manager", "Engineer", "Intern"]
+        }])
+        .then((res) => {
+            console.log(res);
+            switch (res.addEmployee) {
+                case "Manager":
+                    managerPrompt();
+                    break;
+                case "Engineer":
+                    engineerPrompt();
+                    break;
+                case "Intern":
+                    internPrompt();
+                    break;
+            }
+        })
+}
+mainPrompt()
+
+function managerPrompt() {
     inquirer
         .prompt([
             {
@@ -30,54 +54,68 @@ let managerPrompt =
                 type: "input",
                 message: "Team manager's office number: ",
                 name: "officeNumber"
-            }]);
+            }]).then((ans) => {
+                let manager = new Manager(ans.name, ans.id, ans.email, ans.officeNumber)
+                teamMember.push(manager);
+                console.log(teamMember);
+            });
+}
 
-managerPrompt
-    .then((ans) => {
-        let manager = new Manager(ans.name, ans.id, ans.email, ans.officeNumber)
-        teamMember.push(manager);
-    });
+function engineerPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Engineer's name: ",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Engineer's employee id: ",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Engineer's email: ",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Engineer's Github username: ",
+                name: "github"
+            }]).then((ans) => {
+                let engineer = new Engineer(ans.name, ans.id, ans.email, ans.github)
+                teamMember.push(engineer);
+                console.log(teamMember);
+            });
+};
 
-const engineerPrompt = [{
-    type: "input",
-    message: "Engineer's name: ",
-    name: "engineerName"
-},
-{
-    type: "input",
-    message: "Engineer's employee id: ",
-    name: "engineerId"
-},
-{
-    type: "input",
-    message: "Engineer's email: ",
-    name: "engineerEmail"
-},
-{
-    type: "input",
-    message: "Engineer's Github username: ",
-    name: "engineerGithub"
-}];
-
-const internPrompt = [
-    {
-        type: "input",
-        message: "Intern's name: ",
-        name: "internName"
-    },
-    {
-        type: "input",
-        message: "Intern's employee id: ",
-        name: "internId"
-    },
-    {
-        type: "input",
-        message: "Intern's email: ",
-        name: "internEmail"
-    },
-    {
-        type: "input",
-        message: "Intern's school: ",
-        name: "internSchool"
-    }
-];
+function internPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Intern's name: ",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Intern's employee id: ",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Intern's email: ",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Intern's school: ",
+                name: "school"
+            }
+        ]).then((ans) => {
+            let intern = new Intern(ans.name, ans.id, ans.email, ans.school)
+            teamMember.push(intern);
+            console.log(teamMember);
+        });
+};
